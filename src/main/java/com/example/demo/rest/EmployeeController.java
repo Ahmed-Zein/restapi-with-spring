2e26b718c1572f0f1a98547a5a8dd49b8eedb59b
Ahmed Zein <ahmedzein3652@gmail.com>
2024-08-13 03:36:02 +0300

    add: update/delete endpoints

--- src/main/java/com/example/demo/rest/EmployeeController.java
+++ src/main/java/com/example/demo/rest/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.example.demo.rest;
 
-import com.example.demo.dao.EmployeeDAO;
 import com.example.demo.enitity.Employee;
 import com.example.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,16 +22,35 @@ public class EmployeeController {
         return employeeService.findAll();
     }
 
-    @GetMapping("/employees/{id}")
-    public Employee findOne(@PathVariable int id) {
-        Employee employee = employeeService.findById(id);
-        System.out.println(employee);
+    @GetMapping("/employees/{employeeId}")
+    public Employee findOne(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("Employee Id not found - " + employeeId);
+        }
         return employee;
     }
 
     @PostMapping("/employees")
-    public Employee save(Employee employee) {
+    public Employee addEmployee(@RequestBody Employee employee) {
+//        setting the employee id to 0 to ensure a new save instead of update
+        employee.setId(0);
         return employeeService.save(employee);
+    }
+
+    @PutMapping("/employees/{employeeId}")
+    public Employee updateEmployee(@RequestBody Employee employee, @PathVariable int employeeId) {
+        employee.setId(employeeId);
+        return employeeService.save(employee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public void delete(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
     }
 
 }
